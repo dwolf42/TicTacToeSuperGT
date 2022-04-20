cfa6ead89734aab9581f950d3c5ea98e66b8c941
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-04-20 21:35:24 +0200

    scanner.reset() repalced by new Scanner object, corrected method call

--- src/Main.java
+++ src/Main.java
@@ -607,6 +607,6 @@ public class Main {
         // to avoid triggering of scanner.nextLine();
         scanner = new Scanner(System.in);
         scanner.nextLine();
-        howToPlay(board);
+        gameLoop(board);
     }
 }
